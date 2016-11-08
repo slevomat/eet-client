45d437c396733e3595b37975ea5994932d54d884
Jan Langer <jan.langer@gmail.com>
2016-11-08 16:56:46 +0100

    Disable EET integration test on Travis because is blocked

--- tests/SlevomatEET/ClientFunctionalityTest.php
+++ tests/SlevomatEET/ClientFunctionalityTest.php
@@ -10,6 +10,9 @@ class ClientFunctionalityTest extends \PHPUnit\Framework\TestCase
 
 	public function testFunctionality()
 	{
+		if (getenv('TRAVIS') !== false) {
+			$this->markTestSkipped('EET is blocking Travis CI :(');
+		}
 		$crypto = new CryptographyService(__DIR__ . '/../../cert/EET_CA1_Playground-CZ00000019.key', __DIR__ . '/../../cert/EET_CA1_Playground-CZ00000019.pub');
 		$configuration = new Configuration('CZ00000019', '273', '/5546/RO24', new EvidenceEnvironment(EvidenceEnvironment::PLAYGROUND), false);
 		$client = new Client($crypto, $configuration, new GuzzleSoapClientDriver(new \GuzzleHttp\Client()));
